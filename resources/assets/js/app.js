2224d57e96fb401cdb5729a2b6eed1f2eabb48d0
maru <sai.vichet70@gmail.com>
2018-06-04 11:40:02 +0700

    auth push

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -2,7 +2,10 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
-import Buefy from 'buefy';
 
-Vue.use(Buefy);    
 // Vue.component('example-component', require('./components/ExampleComponent.vue'));
+
+var app = new Vue({
+    el: '#app',
+    data: {}
+})
